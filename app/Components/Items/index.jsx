eb1b6a0a2243494dbe50ba7f51a21beb6170cc1b
Bernardo <bernardohmarques@gmail.com>
2023-11-02 19:56:36 -0300

    mudança para carrossel swiper

--- app/Components/Items/index.jsx
+++ app/Components/Items/index.jsx
@@ -2,32 +2,56 @@ import { Carousel } from "antd";
 import Item from "./ItemCard";
 import { Container, ItemsContainer, Titulo } from "./styles";
 import { COLORS } from "../../consts/COLORS";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Items = ({ titulo, items, backgrounded }) => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 4,
-    arrows: true,
-    pauseOnHover: false,
-  };
-
   return (
     <Container
     // style={backgrounded ? { background: COLORS.SOFT_GREEN_TWO } : null}
     >
       <Titulo>{titulo}</Titulo>
       <ItemsContainer>
-        <Carousel {...settings}>
+        <Swiper
+          loop
+          slidesPerView={4}
+          navigation={true}
+          modules={[Navigation]}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+              centeredSlides: true,
+            },
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+              centeredSlides: true,
+            },
+            900: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            1221: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+          }}
+        >
           {items?.map((data, idx) => (
-            <Item
-              descricao={data.titulo}
-              preco={data.precoEmCentavos}
-              img={data.img}
-              key={idx}
-            />
+            <SwiperSlide key={idx}>
+              <Item
+                descricao={data.titulo}
+                preco={data.precoEmCentavos}
+                img={data.img}
+                key={idx}
+              />
+            </SwiperSlide>
           ))}
-        </Carousel>
+        </Swiper>
       </ItemsContainer>
     </Container>
   );
