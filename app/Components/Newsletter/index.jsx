7aa11eaea45c83ee3e73ed01ca1d3beff12ccf0c
Bernardo <bernardohmarques@gmail.com>
2023-11-02 19:56:39 -0300

    add loading

--- app/Components/Newsletter/index.jsx
+++ app/Components/Newsletter/index.jsx
@@ -11,26 +11,29 @@ import {
   Titulo,
 } from "./styles";
 
-const Assinatura = () => {
+const Assinatura = ({ show, handleSignUp }) => {
   return (
     <Modal
-      open
+      open={show}
       centered
       closeIcon={false}
       footer={null}
       width={"1000px"}
       className="newsletter"
+      onCancel={handleSignUp}
     >
       <Container>
         <ImageContainer>
-          <Image src={"NewsletterImage.png"} alt="Imagem do modal" />
+          <img src={"NewsletterImage.png"} alt="Imagem do modal" />
         </ImageContainer>
         <Content>
-          <Image src={"Logo.svg"} alt="Logo" width="45%" />
+          <img src={"Logo.svg"} alt="Logo" width="45%" />
           <Titulo>Junte-se a nós!</Titulo>
           <Descricao>Cadastre-se agora e não perca nenhuma promoção</Descricao>
           <InputEmail placeholder="seuemail@seuprovedor.com" />
-          <BotaoCadastrar type="primary">Cadastrar</BotaoCadastrar>
+          <BotaoCadastrar type="primary" onClick={handleSignUp}>
+            Cadastrar
+          </BotaoCadastrar>
           <AvisoSpam>Não se preocupe, não vamos enviar spam</AvisoSpam>
         </Content>
       </Container>
--- app/page.js
+++ app/page.js
@@ -1,27 +1,55 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CarouselComponent from "./Components/Carousel";
 import Items from "./Components/Items";
 import { getCarousel, getDashboard } from "./store/modules/products/actions";
+import Newsletter from "./Components/Newsletter";
+import LoadingScreen from "./filter/Loading";
 
 export default function DefaultPage() {
-  const { dashboard, carouselData } = useSelector((state) => state.products);
+  const { dashboard, loadingDashboard, successDashboard } = useSelector(
+    (state) => state.products
+  );
+  const [showNewsletter, setShowNewsletter] = useState();
   const dispatch = useDispatch();
+
+  const handleSignUp = () => {
+    setShowNewsletter(false);
+  };
 
   useEffect(() => {
     dispatch(getDashboard());
     dispatch(getCarousel());
   }, []);
+
+  useEffect(() => {
+    successDashboard && setShowNewsletter(true);
+  }, [successDashboard]);
+
   return (
     <>
-      {/* <Assinatura /> */}
-      <CarouselComponent />
-      <Items titulo="Tecidos" items={dashboard?.fabrics} />
-      <Items backgrounded titulo="Automotivos" items={dashboard?.automotive} />
-      <Items titulo="Papéis de parede" items={dashboard?.wallpapers} />
-      <Items backgrounded titulo="Acessórios" items={dashboard?.accessories} />
+      <Newsletter handleSignUp={handleSignUp} show={showNewsletter} />
+      {loadingDashboard ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <CarouselComponent />
+          <Items titulo="Tecidos" items={dashboard?.fabrics} />
+          <Items
+            backgrounded
+            titulo="Automotivos"
+            items={dashboard?.automotive}
+          />
+          <Items titulo="Papéis de parede" items={dashboard?.wallpapers} />
+          <Items
+            backgrounded
+            titulo="Acessórios"
+            items={dashboard?.accessories}
+          />
+        </>
+      )}
     </>
   );
 }
